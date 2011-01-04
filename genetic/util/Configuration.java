7ffdaf356d1c2de6b613f440e9fbe84bbb17e5fd
sarumanromulo <sarumanromulo@gmail.com>
2011-01-03 22:58:18 -0200

    Added missing GPL headers to all source files.

--- genetic/util/Configuration.java
+++ genetic/util/Configuration.java
@@ -1,14 +1,25 @@
+/*
+ * This file is part of DALS - Distributed Artificial Life Simulator.
+ * DALS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DALS.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package genetic.util;
 
 import com.thoughtworks.xstream.XStream;
-import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * This class is store every global configuration of the system, in a way it
